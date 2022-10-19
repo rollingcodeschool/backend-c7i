f072477f0c96a439c553ce39c54a953e021cc3b9
Emilse Arias <emilsearias@gmail.com>
2022-10-19 11:41:19 -0300

    agregamos el index.html como archivo estatico

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 //crear una instancia de express
 const app = express();
@@ -10,7 +13,18 @@ app.listen( app.get('port'),()=>{
 })
 
 //middlewares: son funciones que se ejecutan antes de llegar a las rutas
+app.use(cors()); //permite peticiones remotas
+//permite recibir y usar objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+//informacion extra
+app.use(morgan('dev'))
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname,'../public')))
 
 //rutas
+app.get('/prueba', (req, res)=>{
+    res.send('hola desde el backend en la peticion get')
+})
 
 
