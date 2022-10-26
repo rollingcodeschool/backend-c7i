ef17a4d465eeef93dc462765a9f42547bec5360b
Emilse Arias <emilsearias@gmail.com>
2022-10-26 13:30:05 -0300

    agregamos validaciones con express-validator para crear un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,38 +1,47 @@
+import { validationResult } from "express-validator";
 import Producto from "../models/producto";
 
-export const listarProductos = async(req, res) => {
-  try{
+export const listarProductos = async (req, res) => {
+  try {
     //buscar los productos
     const productos = await Producto.find();
     //responder al frontend con el arreglo de productos
     res.status(200).json(productos);
-  }catch(error){
+  } catch (error) {
     console.log(error);
     //enviar una respuesta al frontend
     res.status(404).json({
-      mensaje: 'Error al buscar los productos'
-    })
+      mensaje: "Error al buscar los productos",
+    });
   }
 };
-export const obtenerProducto = async(req, res) => {
-  try{
+export const obtenerProducto = async (req, res) => {
+  try {
     //obtener el parametro
-    console.log(req.params.id)
+    console.log(req.params.id);
     //buscar en la base de datos el producto que coincide con el parametro
     const productoBuscado = await Producto.findById(req.params.id);
     //responder al frontend
     res.status(200).json(productoBuscado);
-  }catch(error){
+  } catch (error) {
     console.log(error);
     //enviar una respuesta al frontend
     res.status(404).json({
-      mensaje: 'Error al busca un producto'
-    })
+      mensaje: "Error al busca un producto",
+    });
   }
 };
 
 export const crearProducto = async (req, res) => {
   try {
+    // manejar los errores de la validacion
+    const errors = validationResult(req);
+    // errors.isEmpty() devuelve false si hay errores
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
     // console.log(req.body);
     //validar los datos del objeto
     const productoNuevo = new Producto(req.body);
@@ -49,34 +58,58 @@ export const crearProducto = async (req, res) => {
   }
 };
 
-export const editarProducto = async(req, res)=>{
-  try{
+export const editarProducto = async (req, res) => {
+  try {
     //obtener el parametro (req.params.id)
     //obtener los datos del body validados (req.body)
     //actualizar el producto en mi base de datos
     await Producto.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({
-      mensaje: 'El producto fue editado correctamente'
-    })
-  }catch(error){
+      mensaje: "El producto fue editado correctamente",
+    });
+  } catch (error) {
     console.log(error);
     res.status(400).json({
-      mensaje: 'Error al intentar editar un producto'
-    })
+      mensaje: "Error al intentar editar un producto",
+    });
   }
-}
-export const borrarProducto = async(req, res)=>{
-  try{
+};
+export const borrarProducto = async (req, res) => {
+  try {
     //obtener el parametro (req.params.id)
     //borrar el producto de mi base de datos
-    await Producto.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      mensaje: 'El producto fue eliminado correctamente'
-    })
-  }catch(error){
+
+    Producto.findByIdAndDelete(req.params.id, (err,producto)=> {
+      if (err || producto === null) {
+        console.log(err);
+        return res
+          .status(404)
+          .json({ mensaje: "No se pudo eliminar el producto" });
+      }
+      console.log(producto);
+      res.status(200).json({
+        mensaje: "El producto fue eliminado correctamente",
+      });
+    });
+  } catch (error) {
     console.log(error);
     res.status(404).json({
-      mensaje: 'Error al intentar borrar un producto'
-    })
+      mensaje: "Error al intentar borrar un producto",
+    });
   }
-}
+};
+// export const borrarProducto = async(req, res)=>{
+//   try{
+//     //obtener el parametro (req.params.id)
+//     //borrar el producto de mi base de datos
+//     await Producto.findByIdAndDelete(req.params.id)
+//     res.status(200).json({
+//       mensaje: 'El producto fue eliminado correctamente'
+//     })
+//   }catch(error){
+//     console.log(error);
+//     res.status(404).json({
+//       mensaje: 'Error al intentar borrar un producto'
+//     })
+//   }
+// }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -6,6 +6,7 @@ import {
   listarProductos,
   obtenerProducto,
 } from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 //instanciar el router
 const router = Router();
@@ -14,7 +15,36 @@ const router = Router();
 //     res.send('hola desde el backend en la peticion get')
 // })
 
-router.route("/productos").get(listarProductos).post(crearProducto);
+router
+  .route("/productos")
+  .get(listarProductos)
+  .post(
+    [
+      check("nombreProducto", "El nombre del producto es obligatorio")
+        .notEmpty()
+        .isLength({ min: 2, max: 50 })
+        .withMessage("El producto debe tener entre 2 y 50 caracteres"),
+      check("precio", "El precio del producto es obligatorio")
+        .notEmpty()
+        .isNumeric()
+        .withMessage("El precio debe ser numerico")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("El precio debe estar entre 1 y  10000");
+          }
+        }),
+      check("imagen")
+        .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+        .withMessage("Debe enviar una URL valida"),
+      check("categoria")
+        .isIn(["bebida-caliente", "bebida-fria", "dulce", "salado"])
+        .withMessage("La categoria debe ser valida"),
+    ],
+    crearProducto
+  );
+
 router
   .route("/productos/:id")
   .get(obtenerProducto)
